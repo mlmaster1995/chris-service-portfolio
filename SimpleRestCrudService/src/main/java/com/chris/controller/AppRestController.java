7824f75724e344d64ecaefb9be4ed799308e40ee
mlmaster1995 <ykuo2014@outlook.com>
2024-03-26 16:52:37 -0400

    finish controller layer and all test cases

--- SimpleRestCrudService/src/main/java/com/chris/controller/AppRestController.java
+++ SimpleRestCrudService/src/main/java/com/chris/controller/AppRestController.java
@@ -2,6 +2,8 @@ package com.chris.controller;
 
 import com.chris.dto.GymMemberDto;
 import com.chris.service.MemberCrudService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +35,14 @@ public class AppRestController extends BaseController<ResponseEntity> {
 
     private final MemberCrudService _memberCrudService;
 
+    private final ObjectMapper _mapper;
+
     @Autowired
     public AppRestController(
-            @Qualifier(value = MEMBER_SERVICE_BEAN) MemberCrudService appService) {
+            @Qualifier(value = MEMBER_SERVICE_BEAN) MemberCrudService appService, ObjectMapper mapper) {
         super();
         this._memberCrudService = appService;
+        this._mapper = mapper;
     }
 
     @GetMapping("/members")
@@ -46,8 +51,8 @@ public class AppRestController extends BaseController<ResponseEntity> {
         try {
             list = _memberCrudService.findAllMembers();
         } catch (Exception exp) {
-            _LOG.error("fails to find all members at controller layer...: " + exp);
-            return ResponseEntity.badRequest().body(list);
+            _LOG.error("fails to find all members at the controller layer...: " + exp);
+            return ResponseEntity.badRequest().build();
         }
 
         return new ResponseEntity<>(list, HttpStatus.OK);
@@ -55,32 +60,74 @@ public class AppRestController extends BaseController<ResponseEntity> {
 
     @GetMapping("/members/{memberId}")
     public ResponseEntity<GymMemberDto> findMemberById(@PathVariable Integer memberId) {
-        return null;
+        GymMemberDto dto = null;
+
+        try {
+            dto = _memberCrudService.findMemberById(memberId);
+        } catch (Exception exp) {
+            _LOG.error("fails to find the member by id({}) at the controller layer...", memberId);
+            return ResponseEntity.badRequest().build();
+        }
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @PostMapping("/member/email")
-    public ResponseEntity<String> findMemberByEmail(@RequestBody String email) {
-        return null;
+    public ResponseEntity<GymMemberDto> findMemberByEmail(@RequestBody String emailJson) {
+        GymMemberDto dto = null;
+
+        try {
+            JsonNode node = _mapper.readTree(emailJson);
+            String email = node.get("email").asText();
+
+            dto = _memberCrudService.findMemberByEmail(email);
+        } catch (Exception exp) {
+            _LOG.error("fails to find the member by email({}) at the controller layer...", emailJson);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @PostMapping("/member")
     public ResponseEntity<String> saveMember(@RequestBody GymMemberDto member) {
-        return null;
+        try {
+            _memberCrudService.saveMember(member);
+        } catch (Exception exp) {
+            _LOG.error("fails to persist member({}) at the controller layer...", member.toString());
+            return new ResponseEntity<>(exp.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(String.format("member({}) is persisted...", member.toString()), HttpStatus.OK);
     }
 
     /**
-     * body must have id, or it performs as save
+     * body must have id, or it performs as saving
      *
      * @param member
      * @return
      */
     @PutMapping("/member")
-    public ResponseEntity<GymMemberDto> updateMember(@RequestBody GymMemberDto member) {
-        return null;
+    public ResponseEntity<String> updateMember(@RequestBody GymMemberDto member) {
+        try {
+            _memberCrudService.updateMember(member);
+        } catch (Exception exp) {
+            _LOG.error("fails to update member({}) at the controller layer...", member.toString());
+            return new ResponseEntity<>(exp.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(String.format("member({}) is updated...", member.toString()), HttpStatus.OK);
     }
 
     @DeleteMapping("/members/{memberId}")
-    public ResponseEntity<String> deleteMemberById(@PathVariable Integer employeeId) {
-        return null;
+    public ResponseEntity<String> deleteMemberById(@PathVariable Integer memberId) {
+        try {
+            _memberCrudService.deleteMemberById(memberId);
+        } catch (Exception exp) {
+            _LOG.error("fails to delete member with id ({}) at the controller layer...", memberId);
+            return new ResponseEntity<>(exp.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(String.format("member with id ({}) is deleted...", memberId), HttpStatus.OK);
     }
 }
--- SimpleRestCrudService/src/test/java/com/chris/controller/AppRestControllerTest.java
+++ SimpleRestCrudService/src/test/java/com/chris/controller/AppRestControllerTest.java
@@ -1,8 +1,12 @@
 package com.chris.controller;
 
 import com.chris.dto.GymMemberDto;
+import com.chris.exception.AppServiceException;
 import com.chris.service.MemberCrudServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +14,31 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.chris.util.AppBeanConstant.APP_CONTROLLER_BEAN;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -30,7 +47,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class AppRestControllerTest {
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc _mockMvc;
+
+    @Autowired
+    private JdbcTemplate _template;
+
+    @Autowired
+    private ObjectMapper _mapper;
 
     @SpyBean
     private MemberCrudServiceImpl _memberService;
@@ -39,22 +62,153 @@ class AppRestControllerTest {
     @Qualifier(value = APP_CONTROLLER_BEAN)
     private AppRestController _appController;
 
-    //ToDo: add json path
+    @AfterEach
+    public void afterEachTest() {
+        _template.execute("delete from member");
+    }
+
+    @Order(1)
     @Test
-    @Sql("/insert-data.sql")
-    public void getFindAllMembersEp() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/members"))
+    public void testFindMemberById() throws Exception {
+        _template.execute("INSERT INTO `member` (`first_name`,`last_name`,`email`) VALUES ('Neil','Hamilton','neil@chrismember.ca')");
+
+        Integer memberId = 1;
+        doCallRealMethod().when(_memberService).findMemberById(memberId);
+
+        //from controller
+        ResponseEntity<GymMemberDto> entity = _appController.findMemberById(memberId);
+        assertTrue(entity.getStatusCode().value() == 200);
+        assertTrue(entity.getBody().getFirstName().equals("Neil"));
+        assertTrue(entity.getBody().getLastName().equals("Hamilton"));
+        assertTrue(entity.getBody().getEmail().equals("neil@chrismember.ca"));
+
+        //from client
+        _mockMvc.perform(get("/api/members/{memberId}", memberId))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.email", is("neil@chrismember.ca")));
     }
 
-
+    @Order(2)
     @Test
     @Sql("/insert-data.sql")
-    public void getFindAllMembersController() {
+    public void testFindAllMembersEp() throws Exception {
+        //from client
+        _mockMvc.perform(get("/api/members"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(6)));
+
+        //from controller
+        doCallRealMethod().when(_memberService).findAllMembers();
         ResponseEntity<List<GymMemberDto>> ans = _appController.findAllMembers();
+        ans.getBody().stream().forEach(x -> System.out.println("body data: " + x.toString()));
+        System.out.println(ans.toString());
         assertTrue(ans.getStatusCode().value() == 200);
+        assertTrue(ans.getBody().size() == 6);
     }
 
-    //ToDo: test exception handler
+
+    @Order(3)
+    @Test
+    @Sql("/insert-data.sql")
+    public void testFindAllMembersException() throws Exception {
+        doThrow(new AppServiceException("mocked error at service layer")).when(_memberService).findAllMembers();
+
+        //from controller layer
+        ResponseEntity<List<GymMemberDto>> ans = _appController.findAllMembers();
+        assertTrue(ans.getStatusCode().equals(HttpStatus.BAD_REQUEST));
+        assertNull(ans.getBody());
+
+        //from client
+        _mockMvc.perform(MockMvcRequestBuilders.get("/api/members")).andExpect(status().isBadRequest());
+    }
+
+    @Order(4)
+    @Test
+    public void testFindMemberByEmail() throws Exception {
+        _template.execute("INSERT INTO `member` (`first_name`,`last_name`,`email`) VALUES ('Neil','Hamilton','neil@chrismember.ca')");
+
+        String bodyInStr = "{\"email\":\"neil@chrismember.ca\"}";
+        _mockMvc.perform(post("/api/member/email")
+                        .contentType(MediaType.APPLICATION_JSON).content(bodyInStr))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.firstName", is("Neil")))
+                .andExpect(jsonPath("$.lastName", is("Hamilton")))
+                .andExpect(jsonPath("$.email", is("neil@chrismember.ca")));
+    }
+
+    @Order(5)
+    @Test
+    public void testSaveMember() throws Exception {
+        doCallRealMethod().when(_memberService).findMemberByEmail("neil@chrismember.ca");
+
+        GymMemberDto dto = GymMemberDto.builder()
+                .firstName("chris")
+                .lastName("yang")
+                .email("kyang3@lakeheadu.ca")
+                .build();
+
+        _mockMvc.perform(post("/api/member")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(_mapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")));
+
+        dto = _memberService.findMemberByEmail("kyang3@lakeheadu.ca");
+
+        assertTrue(dto.getFirstName().equals("chris"));
+        assertTrue(dto.getLastName().equals("yang"));
+        assertTrue(dto.getEmail().equals("kyang3@lakeheadu.ca"));
+    }
+
+    @Order(6)
+    @Test
+    public void testUpdateMember() throws Exception {
+        _template.execute("INSERT INTO `member` (`first_name`,`last_name`,`email`) VALUES ('Neil','Hamilton','neil@chrismember.ca')");
+
+        doCallRealMethod().when(_memberService).findMemberByEmail("neil@chrismember.ca");
+        GymMemberDto dto = _memberService.findMemberByEmail("neil@chrismember.ca");
+
+        dto.setFirstName("kuo");
+        dto.setLastName("yang");
+        dto.setEmail("ykuo2014@outlook.com");
+
+        _mockMvc.perform(put("/api/member")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(_mapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")));
+
+        dto = _memberService.findMemberByEmail("ykuo2014@outlook.com");
+        assertTrue(dto.getFirstName().equals("kuo"));
+        assertTrue(dto.getLastName().equals("yang"));
+        assertTrue(dto.getEmail().equals("ykuo2014@outlook.com"));
+    }
+
+
+    @Order(7)
+    @Test
+    public void testDeleteMemberById() throws Exception {
+        doCallRealMethod().when(_memberService).findAllMembers();
+
+        _template.execute("INSERT INTO `member` (`first_name`,`last_name`,`email`) VALUES ('Neil','Hamilton','neil@chrismember.ca')");
+
+        List<GymMemberDto> dtos = _memberService.findAllMembers();
+
+        dtos = dtos.stream().filter(x -> x.getEmail().equals("neil@chrismember.ca")).collect(Collectors.toList());
+        assertTrue(dtos.get(0).getEmail().equals("neil@chrismember.ca"));
+
+        int id = dtos.get(0).getId();
+
+        _mockMvc.perform(delete("/api/members/{memberId}", id))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")));
+
+        dtos = _memberService.findAllMembers();
+        assertTrue(dtos.isEmpty());
+    }
+
+
 }
