5c4f54f8a36fb5d354b8c0da2959e3f3270ae306
mlmaster1995 <ykuo2014@outlook.com>
2024-03-30 20:28:25 -0400

    test vpc creat with boto3

--- Backend-Service-Jenkins-Pipeline/boto3/testCreateVPC.py
+++ Backend-Service-Jenkins-Pipeline/boto3/testCreateVPC.py
@@ -1,11 +1,12 @@
 import boto3
+import time
 
 ec2_client = boto3.client('ec2')
 
 
 
-### create vpc
-chris_vpc_name = 'chris-vpc-20240329'
+################## create vpc
+chris_vpc_name = 'chris-vpc-20240330'
 chris_vpc_cidrBlock = '192.168.0.0/16'
 chris_vpc_desc = ec2_client.describe_vpcs(
     Filters=[
@@ -22,7 +23,6 @@ chris_vpc_id = None
 chris_vpcs = chris_vpc_desc.get('Vpcs')
 if not chris_vpcs:
     print(f"vpc with name '{chris_vpc_name}' not exists, and will create one...")
-    
     chris_vpc = ec2_client.create_vpc(
         CidrBlock=chris_vpc_cidrBlock,
         DryRun=False,
@@ -39,6 +39,7 @@ if not chris_vpcs:
         ]
     )
 
+    # wait for creation
     time.sleep(5)
 
     chris_vpc_id = chris_vpc.get('Vpc').get('VpcId')
@@ -49,7 +50,9 @@ else:
 
 
 
-### create igw
+
+
+################## create igw
 chris_igw_name = "chris-vpc-igw"
 chris_igw_desc = ec2_client.describe_internet_gateways(
     Filters=[
@@ -65,6 +68,7 @@ chris_igw_desc = ec2_client.describe_internet_gateways(
 
 chris_igws = chris_igw_desc.get('InternetGateways')
 chris_igw_id = None
+chris_igw_attached = None
 if not chris_igws:
     print(f"igw with name '{chris_igw_name}' not exists, and will create a new one...")
     chris_igw = ec2_client.create_internet_gateway(
@@ -81,34 +85,87 @@ if not chris_igws:
         ],
         DryRun=False
     )
-    chris_igw_id = chris_igw.get('InternetGatewayId')
+
+    # wait for creation
+    time.sleep(5)
+
+    chris_igw_id = chris_igw.get('InternetGateway').get('InternetGatewayId')
     print(f"igw with name '{chris_igw_name}' & id '{chris_igw_id}' is created...")
+
+    chris_igw_attached = False
 else:
     chris_igw_id = chris_igws[0].get('InternetGatewayId')
     print(f"igw with name '{chris_igw_name}' & id '{chris_igw_id}' exists already...")
 
+    chris_igw_attached = chris_igws[0].get('Attachments')[0].get('State') == 'available'
+
+if not chris_igw_attached:
+    chris_igw_attach = ec2_client.attach_internet_gateway(
+        DryRun=False,
+        InternetGatewayId=chris_igw_id,
+        VpcId=chris_vpc_id
+    )
+    print(f"igw with name '{chris_igw_name}' is attached to the vpc with id '{chris_vpc_id}'")
+else: 
+    print(f"igw with name '{chris_igw_name}' is attached already...")
 
 
 
-### create route table
-chris_route_name='chris-route-name'
+
+################## create route table
+chris_route_table_name='chris-vpc-pub-route-table'
 
 chris_route_desc = ec2_client.describe_route_tables(
-    Filters=[
+    Filters = [
         {
             'Name': 'tag:name',
             'Values': [
-                chris_route_name,
+                chris_route_table_name,
             ]
         },
     ],
     DryRun=False
 )
 
-if not chris_igw_desc:
-    ???? 
+chris_route_tables = chris_route_desc.get('RouteTables')
+chris_route_table_id = None
+if not chris_route_tables:
+    print(f"route table with name '{chris_route_table_name} not exists and will create it...'")
 
-### create subnets
+    chris_route_table = ec2_client.create_route_table(
+        DryRun = False,
+        VpcId = chris_vpc_id,
+        TagSpecifications=[
+            {
+                'ResourceType': 'route-table',
+                'Tags': [
+                    {
+                        'Key': 'name',
+                        'Value': chris_route_table_name
+                    },
+                ]
+            },
+        ]
+    )
+
+    time.sleep(5)
+
+    chris_route_table_id = chris_route_table.get('RouteTable').get('RouteTableId')
+    print(f"route table with name '{chris_route_table_name}' and id '{chris_route_table_id}' is created...")
+
+    # add igw to the route table 
+    chris_route = ec2_client.create_route(
+        DestinationCidrBlock='0.0.0.0/0',
+        DryRun=False,
+        GatewayId=chris_igw_id,
+        RouteTableId=chris_route_table_id
+    )
+
+else:
+    chris_route_table_id = chris_route_tables[0].get('RouteTableId')
+    print(f"route table with name '{chris_route_table_name}' and id '{chris_route_table_id}' exists already...")
+
+################## create subnets
 # 
 # RFC1918 range: 
 # 10.0.0.0-10.255.255.255 (10/8 prefix)
@@ -123,12 +180,12 @@ if not chris_igw_desc:
 # VPC ... -> 192.168.0.0/16
 # 
 # 4096-host/subnet:
-# subnet pri us-east-1a ->  192.168.0.0
-# subnet pri us-east-1b ->  192.168.16.0
-# subnet pri us-east-1c ->  192.168.32.0
-# subnet pub us-east-1d ->  192.168.48.0
-# subnet pub us-east-1e ->  192.168.64.0
-# subnet pub us-east-1f ->  192.168.80.0
+# subnet pri us-east-1a ->  192.168.0.0/20
+# subnet pri us-east-1b ->  192.168.16.0/20
+# subnet pri us-east-1c ->  192.168.32.0/20
+# subnet pub us-east-1d ->  192.168.48.0/20
+# subnet pub us-east-1e ->  192.168.64.0/20
+# subnet pub us-east-1f ->  192.168.80.0/20
 # 
 # internet gw -> attach to vpc
 # 
@@ -138,17 +195,17 @@ if not chris_igw_desc:
 # 
 # private route table -> associate the private net -> attach nat to the table
 # 
-chris_subnets=[('pri-us-east-1a','192.168.0.0','us-east-1a'),
-               ('pri-us-east-1b','192.168.16.0','us-east-1b'),
-               ('pri-us-east-1c','192.168.32.0','us-east-1c'),
-               ('pub-us-east-1a','192.168.48.0','us-east-1d'),
-               ('pub-us-east-1b','192.168.64.0','us-east-1e'),
-               ('pub-us-east-1c','192.168.80.0','us-east-1f')]
+chris_subnet_info=[('pri-us-east-1a','192.168.0.0/20','us-east-1a'),
+               ('pri-us-east-1b','192.168.16.0/20','us-east-1b'),
+               ('pri-us-east-1c','192.168.32.0/20','us-east-1c'),
+               ('pub-us-east-1d','192.168.48.0/20','us-east-1d'),
+               ('pub-us-east-1e','192.168.64.0/20','us-east-1e'),
+               ('pub-us-east-1f','192.168.80.0/20','us-east-1f')]
 
-for subnet in chris_subnets:
-    subnet_name = subnet[0]
-    subnet_cidrBlock = subnet[1]
-    subnet_zone = subnet[2]
+for subnet_info in chris_subnet_info:
+    subnet_name = subnet_info[0]
+    subnet_cidrBlock = subnet_info[1]
+    subnet_zone = subnet_info[2]
 
     chris_subnet_desc = ec2_client.describe_subnets(
         Filters=[
@@ -162,9 +219,10 @@ for subnet in chris_subnets:
         DryRun=False,
     )
 
-    chris_subnets_info_list = chris_subnet_desc.get('Subnets')
+    chris_subnets = chris_subnet_desc.get('Subnets')
 
-    if not chris_subnets_info_list:
+    # create subnet if not exists
+    if not chris_subnets:
         print(f"subnet with name '{subnet_name}' not exists, and will create a new one...")
         chris_subnet = ec2_client.create_subnet(
             TagSpecifications=[
@@ -184,11 +242,17 @@ for subnet in chris_subnets:
             DryRun=False
         )
 
-        ?????
+        print(f"subnet with name '{subnet_name}' is created...")
+
+        #if 'pub' in subnet_name:
+        subnet_id = chris_subnet.get('Subnet').get('SubnetId')
+
+        associate_route_table = ec2_client.associate_route_table(
+            DryRun = False,
+            RouteTableId = chris_route_table_id,
+            SubnetId = subnet_id
+        )
+
+        print(f"subnet with name '{subnet_name}' has been associated with route table with name '{chris_route_table_name}'")
     else:
         print(f"subnet with name '{subnet_name}' exists already...")
-
-
-
-
-
