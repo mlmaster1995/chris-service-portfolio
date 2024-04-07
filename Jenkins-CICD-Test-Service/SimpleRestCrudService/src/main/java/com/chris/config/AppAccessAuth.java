09a5285a50def2114c54aa138e469e66617cb9dd
mlmaster1995 <ykuo2014@outlook.com>
2024-04-07 17:57:52 -0400

    add license and a docker file

--- Jenkins-CICD-Test-Service/SimpleRestCrudService/src/main/java/com/chris/config/AppAccessAuth.java
+++ Jenkins-CICD-Test-Service/SimpleRestCrudService/src/main/java/com/chris/config/AppAccessAuth.java
@@ -1,3 +1,27 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2024 Chris Yang
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.chris.config;
 
 import com.chris.auth.AppAuthService;
@@ -51,13 +75,15 @@ public class AppAccessAuth {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(config -> config
-                .requestMatchers(HttpMethod.GET, GET_MEMBERS_ENDPOINT).hasAnyRole(RoleType.USER.getVal(), RoleType.ADMIN.getVal())
-                .requestMatchers(HttpMethod.GET, GET_MEMBER_BY_ID_ENDPOINT).hasAnyRole(RoleType.USER.getVal(), RoleType.ADMIN.getVal())
-                .requestMatchers(HttpMethod.POST, GET_MEMBER_BY_EMAIL_ENDPOINT).hasAnyRole(RoleType.USER.getVal(), RoleType.ADMIN.getVal())
-                .requestMatchers(HttpMethod.POST, POST_MEMBER_ENDPOINT).hasRole(RoleType.ADMIN.getVal()).requestMatchers(HttpMethod.PUT, PUT_MEMBER_ENDPOINT)
-                .hasRole(RoleType.ADMIN.getVal()).requestMatchers(HttpMethod.DELETE, DELETE_MEMBER_ENDPOINT).hasRole(RoleType.ADMIN.getVal())
-                .requestMatchers(HttpMethod.GET, GET_HEALTH_CHECK_ENDPOINT).hasAnyRole(RoleType.USER.getVal(), RoleType.ADMIN.getVal()))
-                .httpBasic(Customizer.withDefaults()).csrf(Customizer.withDefaults());
+                        .requestMatchers(HttpMethod.GET, GET_MEMBERS_ENDPOINT).hasAnyRole(RoleType.USER.getVal(), RoleType.ADMIN.getVal())
+                        .requestMatchers(HttpMethod.GET, GET_MEMBER_BY_ID_ENDPOINT).hasAnyRole(RoleType.USER.getVal(), RoleType.ADMIN.getVal())
+                        .requestMatchers(HttpMethod.POST, GET_MEMBER_BY_EMAIL_ENDPOINT).hasAnyRole(RoleType.USER.getVal(), RoleType.ADMIN.getVal())
+                        .requestMatchers(HttpMethod.POST, POST_MEMBER_ENDPOINT).hasRole(RoleType.ADMIN.getVal())
+                        .requestMatchers(HttpMethod.PUT, PUT_MEMBER_ENDPOINT).hasRole(RoleType.ADMIN.getVal())
+                        .requestMatchers(HttpMethod.DELETE, DELETE_MEMBER_ENDPOINT).hasRole(RoleType.ADMIN.getVal())
+                        .requestMatchers(HttpMethod.GET, GET_HEALTH_CHECK_ENDPOINT).authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf().disable();
 
         return http.build();
     }
