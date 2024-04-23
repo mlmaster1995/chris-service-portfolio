a8f198101dae7afba217074d935a42c88d1a163b
mlmaster1995 <ykuo2014@outlook.com>
2024-04-22 21:23:24 -0400

    add auth user dto test

---
+++ Chris-Auth-Entry-Service/AuthEntryClient/src/test/java/com/chris/dto/AuthUserDtoTest.java
@@ -0,0 +1,9 @@
+package com.chris.dto;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AuthUserDtoTest {
+
+}
